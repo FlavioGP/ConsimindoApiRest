9c6f05c6aa978b3aba4d5adfc8e958a602edc99c
= <flavio.gpinheiro@hotmail.com>
2022-10-10 19:53:06 +0000

    buscando alunos da API

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -56,7 +56,7 @@ export default createGlobalStyle`
 `;
 
 export const Container = styled.section`
-  max-width: 360px;
+  max-width: 480px;
   margin: 30px auto;
   background: #fff ;
   padding: 30px;
