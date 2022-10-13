c889aec88d2d27d495d1992ade9a5ea2b4deaba2
= <flavio.gpinheiro@hotmail.com>
2022-10-13 20:06:03 +0000

    Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,60 @@
 import React from "react";
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch } from 'react-redux';
+import { get } from 'lodash';
 
-import { Container } from '../../styles/GlobalStyles'
+import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
 
+export default function Login(props){
+  const dispatch = useDispatch();
 
-export default function Login(){
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if(!isEmail(email)){
+      formErrors = true;
+      toast.error('Email invalido.');
+    }
+
+    if(password.length < 6 || password.length > 50){
+      formErrors = true;
+      toast.error('Senha invalida.');
+    }
+
+    if(formErrors) console.log(formErrors);
+
+    dispatch(actions.loginRequest({ email, password, prevPath}));
+
+  };
+
   return(
     <Container>
       <h1>Login</h1>
+
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+        <button type="submit">Login</button>
+      </Form>
     </Container>
   );
 }
---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,41 @@
+import { call, put, all, takeLatest } from 'redux-saga/effects';
+import {toast} from 'react-toastify';
+import { get } from 'lodash';
+
+import axios from '../../../services/axios';
+import history from '../../../services/history';
+
+import * as actions from './actions';
+import * as types from '../types';
+
+
+function* LoginRequest({ payload }) {
+  try{
+    const response = yield call(axios.post, '/tokens', payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.success('Login realizado!');
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
+
+  }catch(e){
+    toast.error('Usuario ou senha invalidos');
+
+    yield put(actions.loginFailure());
+  }
+
+}
+
+function persistRehydrate({ payload }){
+  const token = get(payload, 'auth.token', '');
+  if(!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, LoginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate)
+]);
